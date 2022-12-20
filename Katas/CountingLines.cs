e70d36941100dc55728d07de52e2eddc1b1af979
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2022-12-20 16:32:19 +0000

    Adding counting lines kata

---
+++ Katas/CountingLines.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Katas
+{
+    public class CountingLines
+    {
+        public string CountLines()
+        {
+            var result = new StringBuilder();
+            var fileList = new List<string> { ".\\Files/TextFile1.txt", ".\\Files/TextFile2.txt" };
+
+            try
+            {
+                fileList.ForEach(file =>
+                    result.AppendLine($"There are {CountCodeLines(File.ReadAllLines(file))} lines of code for: {file}"));
+                return result.ToString();
+            }
+            catch
+            (Exception exception)
+            {
+                return ($"Failed to load or read files: Error: {exception.Message}");
+            }
+        }
+
+        int CountCodeLines(IEnumerable<string> fileLines)
+        {
+            return fileLines.Count(line =>
+                !string.IsNullOrWhiteSpace(line) &&
+                !line.TrimStart().StartsWith('/') &&
+                !line.TrimStart().StartsWith('*'));
+        }
+    }
+}
---
+++ KatasTests/CountingLinesTests.cs
@@ -0,0 +1,23 @@
+﻿using Katas;
+using NUnit.Framework;
+
+namespace KatasTests
+{
+    [TestFixture]
+    public class CountingLinesTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var sut = new CountingLines();
+            var result = sut.CountLines();
+            var expectedResult = "There are 3 lines of code for: .\\Files/TextFile1.txt\r\nThere are 5 lines of code for: .\\Files/TextFile2.txt\r\n";
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
