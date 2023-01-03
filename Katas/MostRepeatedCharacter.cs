b65213f8ce68c947f8df5751917dfc7c5bb64827
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-01-03 23:33:25 +0000

    Adding latest Most-Repeated-Characters kata

---
+++ Katas/MostRepeatedCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Katas
+{
+    public static class MostRepeatedCharacter
+    {
+        public static Tuple<char?, int> LongestRepetition(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return new Tuple<char?, int>(null, 0);
+
+            const string pattern = @"(\w)\1*";
+            var expression = new Regex(pattern);
+            var matches = expression.Matches(input);
+            var result = matches.Where(i => i.Value.Length == matches.Max(x => x.Value.Length)).ToList();
+
+            return new Tuple<char?, int>(result.FirstOrDefault()?.Value.First(), result.FirstOrDefault().Value.Length);
+        }
+
+        public static Tuple<char?, int> LongestRepetitionTwo(string input)
+            => input
+               .Select((x, i) => new Tuple<char?, int>(x, input.Substring(i).TakeWhile(y => y == x).Count()))
+               .OrderByDescending(x => x.Item2)
+               .FirstOrDefault()
+               ?? new Tuple<char?, int>(null, 0);
+    }
+}
---
+++ KatasTests/MostRepeatedCharacterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Katas;
+using NUnit.Framework;
+
+namespace KatasTests
+{
+    [TestFixture]
+    public class MostRepeatedCharacterTest
+    {
+        [Test]
+        [TestCase("aaaabbbbbbbaa", 'b', 7)]
+        public void Test2(string input, char letter, int count)
+        {
+            var result = MostRepeatedCharacter.LongestRepetitionTwo(input);
+            TestContext.WriteLine($"result: {result}");
+            Assert.AreEqual(result.Item1, letter);
+            Assert.AreEqual(result.Item2, count);
+        }
+        
+    }
+}
