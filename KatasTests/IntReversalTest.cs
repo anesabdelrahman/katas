f4ba0c3433e3b1a3c4ff884ad067cf7c5b1bdc71
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-01-22 13:53:36 +0000

    Updating KillerGarageDoor and adding string and int reversal

---
+++ KatasTests/IntReversalTest.cs
@@ -0,0 +1,20 @@
+﻿using Katas;
+using NUnit.Framework;
+
+namespace KatasTests
+{
+    [TestFixture]
+    public class IntReversalTest
+    {
+        [Test]
+        //[TestCase(51, 15)]
+        //[TestCase(987, 789)]
+        //[TestCase(105, 501)]
+        [TestCase(-35, -53)]
+        public void Test(int input, int expected)
+        {
+            var result = new IntReversal().Reverse(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
