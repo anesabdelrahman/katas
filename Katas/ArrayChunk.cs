689b811427edd3abf0183c602f511a19367a62c3
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-01-31 08:13:54 +0000

    Adding Array Chunking

---
+++ Katas/ArrayChunk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Katas
+{
+    public class ArrayChunk
+    {
+        public List<List<int>> Chunk(int[] input, int size)
+        {
+            var result = new List<List<int>>();
+            var index = 0;
+
+            while (index < input.Length)
+            {
+                var current = input.ToList().Skip(index).Take(size).ToList();
+                result.Add(current);
+                index += size;
+            }
+
+            
+
+            return result;
+        }
+    }
+}
