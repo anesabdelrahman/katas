848a0e0122153f6e31b9ea0befdaf0524bb684ad
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-02 08:37:31 +0000

    Adding String Anagram

---
+++ Katas/StringAnagram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Katas
+{
+    public class StringAnagram
+    {
+        public bool IsAnagram(string firstString, string secondString)
+        {
+
+            var firstOutput =  Regex.Replace(firstString.ToLower(), "[^\\w]", "").ToCharArray();
+            var secondOutput = Regex.Replace(secondString.ToLower(), "[^\\w]", "").ToCharArray();
+            //Console.WriteLine(firs);
+            Array.Sort(firstOutput);
+            Array.Sort(secondOutput);
+            return string.Join( ' ', firstOutput) == string.Join(' ', secondOutput);
+        }
+    }
+}
---
+++ KatasTests/StringAnagramTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Katas;
+using NUnit.Framework;
+
+namespace KatasTests
+{
+    [TestFixture]
+    public class StringAnagramTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase("hello#@", "llohe", true)]
+        [TestCase("Whoa! Hi!", "Hi! Whoa!", true)]
+        [TestCase("One One", "Two two two", false)]
+        [TestCase("One one", "One one c", false)]
+        [TestCase("A tree, a life, a bench", "A tree, a fence, a yard", false)]
+        public void Test(string firstString, string secondString, bool expected)
+        {
+            var result = new StringAnagram().IsAnagram(firstString, secondString);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
