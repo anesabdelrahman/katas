5758664435dc286a99c687f5c08b201a404fbe9a
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-03 16:29:40 +0000

    Adding Capitalize First Char of String.

---
+++ Katas/CapitalizeFirstChar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Katas
+{
+    public class CapitalizeFirstChar
+    {
+        public string Capitalize(string input)
+        {
+            input.Split(' ');
+            var result = input.Split(' ').Select(item => item[0].ToString().ToUpper() + item[1..]);
+
+            return string.Join(" ", result);
+        }
+    }
+}
--- Katas/StringAnagram.cs
+++ Katas/StringAnagram.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Katas
@@ -13,7 +10,6 @@ namespace Katas
 
             var firstOutput =  Regex.Replace(firstString.ToLower(), "[^\\w]", "").ToCharArray();
             var secondOutput = Regex.Replace(secondString.ToLower(), "[^\\w]", "").ToCharArray();
-            //Console.WriteLine(firs);
             Array.Sort(firstOutput);
             Array.Sort(secondOutput);
             return string.Join( ' ', firstOutput) == string.Join(' ', secondOutput);
