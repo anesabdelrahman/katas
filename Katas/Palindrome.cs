13be8a0ad0db6cc5151254f2a22dcf25615f2861
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-06 08:53:08 +0000

    Adding Palindrome kata

---
+++ Katas/Palindrome.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Katas
+{
+    public class Palindrome
+    {
+        public bool IsPalindrome(string input)
+        {
+            var result = new StringBuilder();
+            input.ToList().ForEach(x => result.Insert(0, x));
+            return input == result.ToString();
+        }
+    }
+}
---
+++ KatasTests/PalindromeTest.cs
@@ -0,0 +1,27 @@
+﻿using Katas;
+using NUnit.Framework;
+
+namespace KatasTests
+{
+    [TestFixture]
+    public class PalindromeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase("ANA", true)]
+        [TestCase("aaaa", true)]
+        [TestCase("112211", true)]
+        [TestCase("Home", false)]
+        [TestCase("pennep", true)]
+        [TestCase("Fish hsif", false)]
+        public void Test(string input, bool expected)
+        {
+            var result = new Palindrome().IsPalindrome(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
