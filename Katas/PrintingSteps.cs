8be4a58c0fdd97bdcee7f472623c01922c970986
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-06 08:26:00 +0000

    Adding Printing statements kata

---
+++ Katas/PrintingSteps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Katas
+{
+    public class PrintingSteps
+    {
+        public void Print(int steps)
+        {
+            for (var i = 1; i <= steps; i++)
+            {
+                var str1 = new string('#', i);
+                var str2 = new string(' ', steps-i);
+
+                var current = string.Concat(str1, str2);
+                Console.WriteLine(current);
+            }
+        }
+    }
+}
