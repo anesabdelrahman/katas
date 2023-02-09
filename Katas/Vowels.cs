acc7fc331d72480524000f8b0abc4f00db68abfb
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-09 08:24:40 +0000

    Adding Vowels Kata

---
+++ Katas/Vowels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Katas
+{
+    public class Vowels
+    {
+        public int CountVowels(string input)
+        {
+            var match = Regex.Matches(input, "[aeiou]", RegexOptions.IgnoreCase);
+            return match.Count;
+
+            
+        }
+    }
+}
+// One Solution
+//var result = 0;
+//var vowels = new[] { 'a', 'e', 'i', 'o', 'u' };
+
+//foreach (var vowel in input.ToLower())
+//{
+//    if (vowels.Contains(vowel))
+//    {
+//        result++;
+//    }
+//}
+
+//return result;
