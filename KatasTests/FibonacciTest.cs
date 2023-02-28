2129eea5afe9682f7241a97e343148f37cfe5a49
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-28 08:21:45 +0000

    Adding Fibonacci kata

---
+++ KatasTests/FibonacciTest.cs
@@ -0,0 +1,27 @@
+﻿using Katas;
+using NUnit.Framework;
+
+namespace KatasTests
+{
+    [TestFixture]
+    public class FibonacciTest
+    {
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [TestCase(6, 8)]
+        [TestCase(7, 13)]
+        [TestCase(8, 21)]
+        [TestCase(9, 34)]
+        [TestCase(15, 610)]
+        public void Test(int input, int expected)
+        {
+            var result = new Fibonacci().Calculate(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
