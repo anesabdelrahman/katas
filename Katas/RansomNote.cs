7b66dd9188af4c75c5636a99f3c2424a49d74aa4
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-04-19 09:41:25 +0100

    Added RansomNote kata

---
+++ Katas/RansomNote.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Katas
+{
+    public class RansomNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            var magazineLetters = new Dictionary<char, int>();
+            foreach (var m in magazine)
+            {
+                if (magazineLetters.ContainsKey(m))
+                {
+                    magazineLetters[m]++;
+                }
+                else
+                {
+                    magazineLetters.Add(m, 1);
+                }
+            }
+
+            foreach (var r in ransomNote)
+            {
+                if (magazineLetters.TryGetValue(r, out var count))
+                {
+                    if (count > 0)
+                    {
+                        magazineLetters[r]--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
