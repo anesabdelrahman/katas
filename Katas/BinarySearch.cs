b7132562172192921af521e39fe5aaa81ae416bf
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-04-19 09:36:32 +0100

    Added Binary Search functionality.

---
+++ Katas/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace Katas
+{
+    public class BinarySearch
+    {
+        public int Search(int[] sortedArray, int target)
+        {
+            var left = 0;
+            var right = sortedArray.Length -1;
+
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+                if (sortedArray[mid] == target)
+                {
+                    return sortedArray[mid];
+                }
+
+                if (sortedArray[mid] < target)
+                {
+                    left = mid+1;
+
+                }
+                else
+                {
+                    right = mid-1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ KatasTests/Katas/BinarySearchTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Katas;
+using NUnit.Framework;
+
+namespace KatasTests.Katas
+{
+    [TestFixture]
+    public class BinarySearchTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, 6, 6)]
+        public void Test(int[] arr, int target, int expectedResult)
+        {
+            var sut = new BinarySearch();
+            var result = sut.Search(arr, target);
+            TestContext.WriteLine($"result: {result}");
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
