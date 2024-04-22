cd90f8e6cd633563e67a2ac95e79a75edd2102cd
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-04-22 15:10:50 +0100

    Added Anagram kata

---
+++ Katas/Anagram.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas
+{
+    public class Anagram
+    {
+        public bool IsAnagramUsingLists(string first, string second)
+        {
+            if (first.Length != second.Length) return false;
+            var firstLetters = first.OrderBy(x => x).ToList();
+            var secondLetters = second.OrderBy(x => x).ToList();
+
+            return firstLetters.SequenceEqual(secondLetters);
+        }
+
+
+        public bool IsAnagramUsingDictionary(string first, string second)
+        {
+            if (first.Length != second.Length) return false;
+            var charList = new Dictionary<char, int>();
+            foreach (var character in first)
+            {
+                if (charList.ContainsKey(character))
+                {
+                    charList[character]++;
+                }
+                else
+                {
+                    charList[character] = 1;
+                }
+            }
+            
+            foreach (var character in second)
+            {
+                if (!charList.ContainsKey(character))
+                {
+                    return false;
+                }
+
+                charList[character]--;
+                if (charList[character] < 0)
+                    return false;
+            }
+
+            return charList.Values.All(value => value == 0);
+        }
+    }
+}
