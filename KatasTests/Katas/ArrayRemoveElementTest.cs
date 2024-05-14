fdf116d4b29f3da99535f003ea3a9d18ca7ffb03
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-05-14 07:58:27 +0100

    Remove an element from Array

---
+++ KatasTests/Katas/ArrayRemoveElementTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Katas;
+using NUnit.Framework;
+
+namespace KatasTests.Katas
+{
+    [TestFixture]
+    public class ArrayRemoveElementTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(new int[] { 3, 2, 2, 3 }, 3, 2)]
+        public void Test_Not_Efficiently(int[] arr, int target, int expectedResult)
+        {
+            var sut = new ArrayRemoveElements();
+            var result = sut.RemoveElement_Not_Efficient(arr, target);
+            Assert.AreEqual(result, expectedResult);
+        }
+        
+        [Test]
+        [TestCase(new int[] { 3, 2, 2, 3 }, 3, 2)]
+        public void Test_Efficiently(int[] arr, int target, int expectedResult)
+        {
+            var sut = new ArrayRemoveElements();
+            var result = sut.RemoveElement_Efficient(arr, target);
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
