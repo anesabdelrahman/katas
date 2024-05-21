608b62b129d460d1900a95351afd100968a73b9a
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-05-21 08:29:10 +0100

    Finding missing integer in an array kata.

---
+++ Katas/MissingIntegerInArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Katas;
+
+public class MissingIntegerInArray
+{
+    public int CalculateMissingInteger(int[] A)
+    {
+        if (A == null || A.Length == 0)
+        {
+            return 1;
+        }
+
+        Array.Sort(A);
+
+        if (A[0] > 1)
+        {
+            return 1;
+        }
+
+        for (var i = 1; i < A.Length; i++)
+        {
+            if (A[i] == A[i - 1])
+            {
+                continue;
+            }
+
+            // If there's a gap greater than 1 between consecutive elements
+            if (A[i] <= A[i - 1] + 1) continue;
+            if (A[i - 1] + 1 > 0)
+            {
+                return A[i - 1] + 1;
+            }
+        }
+        return A[^1]+1 > 0 ? A[^1] + 1 : 1;
+    } 
+}
---
+++ KatasTests/Katas/MissingIntegerInArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using Katas;
+using NUnit.Framework;
+
+namespace KatasTests.Katas
+{
+    [TestFixture]
+    public class MissingIntegerInArrayTest
+    {
+        [Test]
+        [TestCase(new[] { 1, 3, 6, 4, 1, 2 }, 5)]
+        [TestCase(new[] { -1, -3 }, 1)]
+        public void CalculatePathCost(int[] input, int expected)
+        {
+            var result = new MissingIntegerInArray().CalculateMissingInteger(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
