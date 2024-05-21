db0562f0b86f938213fb4d26639e0afe28756902
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-05-21 08:29:50 +0100

    Finding the shortest path cost kata.

---
+++ Katas/PathMinPathCost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Katas;
+
+public class PathMinPathCost
+{
+    public int CalculatePathCost(int[] array)
+    {
+        var maxCost = 0;
+
+        for (var i = 0; i < array.Length-2; i++)
+        {
+            var currentCost = Math.Abs(array[i+2] - array[i]);
+            maxCost = Math.Max(maxCost, currentCost);
+        }
+
+        return maxCost;
+    } 
+}
+
+
+//index value
+//0     5
+//1     4
+//2     2
---
+++ KatasTests/Katas/PathMinPathCostTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Katas;
+using NUnit.Framework;
+
+namespace KatasTests.Katas
+{
+    [TestFixture]
+    public class PathMinPathCostTest
+    {
+        [Test]
+        //[TestCase(new[] { 1, 3, 5, 8, 12 }, 7)]
+        [TestCase(new[] { 0, 2, 5, 6, 7 }, 5)]
+        //[TestCase(new[] { 0, 3, 9 }, 9)]
+        public void CalculatePathCost(int[] input, int expected)
+        {
+            var result = new PathMinPathCost().CalculatePathCost(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
