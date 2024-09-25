f77e9cc5416f6f19b164ce68f2b2aab7dcb1e1b7
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-09-25 07:51:24 +0100

    Added LeetCode training folders and first challenge The sum of one dimensional array

---
+++ Katas/LeetCode/RunningSumOneDimensionalArray.cs
@@ -0,0 +1,19 @@
+﻿namespace Katas.LeetCode
+{
+    public class RunningSumOneDimensionalArray
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            var currentValue = 0;
+            var result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                currentValue += nums[i];
+                result[i] = currentValue;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ KatasTests/Katas/LeetCode/ArrayChunkTest.cs
@@ -0,0 +1,36 @@
+﻿using Katas.LeetCode;
+using NUnit.Framework;
+
+namespace Tests.Katas.Arrays
+{
+    [TestFixture]
+    public class RunningSumOneDimensionalArrayTest
+    {
+        /// <summary>
+        /// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+        //Return the running sum of nums.
+
+        //Input: nums = [1, 2, 3, 4]
+        //Output: [1,3,6,10]
+
+        //Input: nums = [1, 1, 1, 1, 1]
+        //Output: [1,2,3,4,5]
+
+        //Input: nums = [3, 1, 2, 10, 1]
+        //Output: [3,4,6,16,17]
+        /// </summary>
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4 }, new int[] { 1, 3, 6, 10 })]
+        [TestCase(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 3, 1, 2, 10, 1 }, new int[] { 3, 4, 6, 16, 17 })]
+        public void Test1(int[] input, int[] expected)
+        {
+            var sut = new RunningSumOneDimensionalArray();
+            var result = sut.RunningSum(input);
+            Assert.AreEqual(result, expected);
+        }
+
+    }
+}
