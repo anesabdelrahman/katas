0f3705b59b2360e2c37d036cd400cc60924e3e9c
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-09-28 14:18:40 +0100

    Added MiddleOfLinkedList challenge from LeetCode.

---
+++ Katas/LeetCode/MiddleOfLinkedList.cs
@@ -0,0 +1,63 @@
+﻿namespace Katas.LeetCode
+{
+    public class MiddleOfLinkedList
+    {
+        public Node MiddleNode(LinkedList head)
+        {
+            Node middle = head.Head;
+            Node end = head.Head;
+
+            while (end?.Next != null)
+            {
+                middle = middle.Next;
+                end = end.Next.Next;
+            }
+
+            return middle;
+        }
+    }
+
+    public class Node(int x)
+    {
+        public int Value = x;
+        public Node Next;
+    }
+
+    // A linked list class
+    public class LinkedList
+    {
+        public Node Head;
+        
+        public void Insert(int x)
+        {
+            var temp = new Node(x);
+
+            if (Head == null)
+            {
+                Head = temp;
+                return;
+            }
+
+            Node t = Head;
+            while (t.Next != null)
+            {
+                t = t.Next;
+            }
+
+            t.Next = temp;
+        }
+        public bool AreNodesEqual(Node node1, Node node2)
+        {
+            while (node1 != null && node2 != null)
+            {
+                if (node1.Value != node2.Value)
+                    return false;
+
+                node1 = node1.Next;
+                node2 = node2.Next;
+            }
+
+            return node1 == null && node2 == null;
+        }
+    }
+}
---
+++ KatasTests/LeetCode/MiddleOfLinkedListTest.cs
@@ -0,0 +1,49 @@
+﻿using Katas.LeetCode;
+using NUnit.Framework;
+
+namespace Tests.LeetCode
+{
+    [TestFixture]
+    public class MiddleOfLinkedListTest
+    {
+        /// <summary>
+        //Given the head of a singly linked list, return the middle node of the linked list.
+        //    If there are two middle nodes, return the second middle node.
+        //    Example 1:
+        //Input: head = [1, 2, 3, 4, 5]
+        //Output: [3,4,5]
+        //Explanation: The middle node of the list is node 3.
+        //Example 2:
+        //Input: head = [1, 2, 3, 4, 5, 6]
+        //Output: [4,5,6]
+        //Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+        //    Constraints:
+
+        //The number of nodes in the list is in the range [1, 100].
+        //    1 <= Node.val <= 100
+         /// </summary>
+
+         [Test]
+        [TestCase(new[] {1}, new[] {1})]
+        [TestCase(new[] {1, 2, 3, 4, 5}, new[] {3, 4, 5})]
+        [TestCase(new[] {1, 2, 3, 4, 5, 6}, new[] {4, 5, 6})]
+        public void Test(int[] input, int[] expected)
+        {
+            var linkedList = new LinkedList();
+            var expLinkedList = new LinkedList();
+            foreach (var item in input)
+            {
+                linkedList.Insert(item);
+            }
+            foreach (var item in expected)
+            {
+                expLinkedList.Insert(item);
+            }
+            var sut = new MiddleOfLinkedList();
+            var result = sut.MiddleNode(linkedList);
+            Assert.IsTrue(linkedList.AreNodesEqual(result, expLinkedList.Head));
+        }
+    }
+
+    
+}
