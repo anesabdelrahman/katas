c7290fbcc93f1b109f364bdc6b2abd38c25efb4d
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-09-30 08:15:51 +0100

    Added Merge-Sort-Two-Arrays challenge from LeetCode.

---
+++ Katas/Arrays/MergeSortTwoArrays.cs
@@ -0,0 +1,34 @@
+﻿namespace Katas.Arrays
+{
+    public class MergeSortTwoArrays
+    {
+        public int[] MergeSort(int[] nums1, int[] nums2, int firstNumsLength)
+        {
+            var firstZeroIndex = firstNumsLength;
+            foreach (var item in nums2)
+            {
+                for (var j = 0; j < nums1.Length; j++)
+                {
+
+                    if (j == firstZeroIndex)
+                    {
+                        nums1[firstZeroIndex] = item;
+                        firstZeroIndex++;
+                        break;
+                    }
+
+                    if (item >= nums1[j]) continue;
+                    for (var k = nums1.Length - 1; k > j; k--)
+                    {
+                        nums1[k] = nums1[k - 1];
+
+                    }
+                    nums1[j] = item;
+                    firstZeroIndex++;
+                    break;
+                }
+            }
+            return nums1;
+        }
+    }
+}
---
+++ KatasTests/LeetCode/MergeSortTwoArraysTest.cs
@@ -0,0 +1,23 @@
+﻿using Katas.Arrays;
+using NUnit.Framework;
+
+namespace Tests.LeetCode
+{
+    [TestFixture]
+    public class MergeSortTwoArraysTest
+    {
+        [Test]
+        [TestCase(new[] { 1, 2, 3, 0, 0, 0 }, new[] { 2, 5, 6 }, 3, new[] { 1, 2, 2, 3, 5, 6 })]
+        [TestCase(new[] { -1, 0, 0, 3, 3, 3, 0, 0, 0 }, new[] { 1, 2, 2 }, 6,  new[] { -1, 0, 0, 1, 2, 2, 3, 3, 3 })]
+        public void Test(int[] nums1, int[] nums2,int firstNumsLength, int[] expected)
+        {
+            var sut = new MergeSortTwoArrays();
+            var result = sut.MergeSort(nums1, nums2, firstNumsLength);
+            foreach (var item in result)
+            {
+                TestContext.Write($"{item}, ");
+            }
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
