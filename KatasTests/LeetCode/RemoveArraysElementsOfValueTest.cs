881bf9a02041d52053f5a2f27bd8bbd1a34e5ac1
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-10-15 12:25:20 +0100

    Remove elements of value from an array

---
+++ KatasTests/LeetCode/RemoveArraysElementsOfValueTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Tests.LeetCode
+{
+    [TestFixture]
+    public class RemoveArraysElementsOfValueTest
+    {
+        [Test]
+        [TestCase(new[] { 3, 2, 2, 3 }, 3, 2)]
+        public void Test(int[] input, int value, int expected)
+        {
+            var sut = new RemoveArraysElementsOfValue();
+            var result = sut.RemoveElement(input, value);
+            TestContext.WriteLine($"result: {result}");
+            Assert.AreEqual(result, expected);
+        }
+    }
+
+    public class RemoveArraysElementsOfValue
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            var result = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val) continue;
+                nums[result] = nums[i];
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
