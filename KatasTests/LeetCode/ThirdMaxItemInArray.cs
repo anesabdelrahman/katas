26fe9867f4a7c0453067709fb7ecf23e9cb7a945
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-10-15 12:26:00 +0100

    Get max third element in an array.

---
+++ KatasTests/LeetCode/ThirdMaxItemInArray.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Tests.LeetCode
+{
+    [TestFixture]
+    public class ThirdMaxItemInArrayTest
+    {
+        [Test]
+        [TestCase(new[] { 3, 2, 1 }, 1)]
+        [TestCase(new[] { 1, 2 }, 2)]
+        [TestCase(new[] { 2, 2, 3, 1 }, 1)]
+        public void Test(int[] input, int expected)
+        {
+            var sut = new ThirdMaxItemInArray();
+            var result = sut.ThirdMaxUsingHashset_More_Efficient(input);
+            TestContext.WriteLine($"result: {result}");
+            Assert.AreEqual(result, expected);
+        }
+    }
+
+    public class ThirdMaxItemInArray
+    {
+        public int ThirdMaxUsingLinqs_Less_Efficient(int[] nums)
+        {
+            var distinct = nums.Distinct().OrderByDescending(x => x).ToList();
+            return distinct.Count > 2 ? distinct.Take(3).Last() : distinct.Max();
+        }
+
+        public int ThirdMaxUsingHashset_More_Efficient(int[] nums)
+        {
+            var max = int.MinValue;
+            var distinctSet = new HashSet<int>();
+
+            foreach (var item in nums)
+            {
+                distinctSet.Add(item);
+                if (item >  max) max = item;
+            }
+
+            var distinctList = distinctSet.ToList();
+            distinctList.Sort();
+            return distinctSet.Count <= 2 ? max : distinctList[^3];
+        }
+    }
+}
