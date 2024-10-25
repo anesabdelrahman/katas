2c44f458ba024c912c795a5be173bfd4d41d94f6
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-10-25 08:31:11 +0100

    This is my playground!

--- ApplicationRunner/Program.cs
+++ ApplicationRunner/Program.cs
@@ -8,6 +8,7 @@ namespace ApplicationRunner
         {
             //var hash = SecurityUtility.EncodePassword("ouEQZx6VbaljTS7X6SoIQ0GQk");
             //var security1 = SecurityUtility.VerifyPasswordHash(hash, "ouEQZx6VbaljTS7X6SoIQ0GQk");
+            new Derived();
             Console.ReadLine();
         }
 
@@ -39,6 +40,31 @@ namespace ApplicationRunner
         public TestingReadOnly()
         {
             //VERSION = 6;
+        }
+    }
+
+    class Foo
+    {
+        public Foo(string s)
+        {
+            Console.WriteLine("Foo constructor: {0}", s);
+        }
+        public void Bar() { }
+    }
+    class Base
+    {
+        readonly Foo baseFoo = new Foo("Base initializer");
+        public Base()
+        {
+            Console.WriteLine("Base constructor");
+        }
+    }
+    class Derived : Base
+    {
+        readonly Foo derivedFoo = new Foo("Derived initializer");
+        public Derived()
+        {
+            Console.WriteLine("Derived constructor");
         }
     }
 }
