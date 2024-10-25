680078870cef0c0ccc42f383f89bae7c531fee4c
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2024-10-25 08:29:53 +0100

    An update to this kata to use Regex instead (WIP still)

--- Katas/Miscellaneous/CountingLines.cs
+++ Katas/Miscellaneous/CountingLines.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Katas.Miscellaneous
 {
@@ -11,12 +12,12 @@ namespace Katas.Miscellaneous
         public string CountLines()
         {
             var result = new StringBuilder();
-            var fileList = new List<string> { ".\\Files/TextFile1.txt", ".\\Files/TextFile2.txt" };
+            var fileList = new List<string> {".\\Files/TextFile2.txt"};//, ".\\Files/TextFile2.txt" };
 
             try
             {
                 fileList.ForEach(file =>
-                    result.AppendLine($"There are {CountCodeLines(File.ReadAllLines(file))} lines of code for: {file}"));
+                    result.AppendLine($"There are {CountCodeLines_Regex(File.ReadAllLines(file))} lines of code for: {file}"));
                 return result.ToString();
             }
             catch
@@ -26,12 +27,19 @@ namespace Katas.Miscellaneous
             }
         }
 
-        int CountCodeLines(IEnumerable<string> fileLines)
+        int CountCodeLines_Trim(IEnumerable<string> fileLines)
         {
             return fileLines.Count(line =>
                 !string.IsNullOrWhiteSpace(line) &&
                 !line.TrimStart().StartsWith('/') &&
                 !line.TrimStart().StartsWith('*'));
+        }
+        int CountCodeLines_Regex(IEnumerable<string> fileLines)
+        {
+            var regex = new Regex(@"^\s*(\/|\*|\s$)gm");
+
+            // Count lines that do not match the regex (i.e., lines that have content)
+            return fileLines.Count(line => !regex.IsMatch(line.TrimStart()));
         }
     }
 }
